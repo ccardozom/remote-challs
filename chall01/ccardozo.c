62841f8a7942c139f5a65025341f5cd6405a8dd6
Carlos Cardozo <54796906+ccardozom@users.noreply.github.com>
2020-04-03 23:22:31 +0200

    call01 update

--- chall01/ccardozo.c
+++ chall01/ccardozo.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 int ft_strlen(char *str)
 {
 	int i;
@@ -14,19 +10,18 @@ int ft_strlen(char *str)
 
 int     hv_necklace(char *s1, char *s2)
 {
-	char *temp;
-	int	index1, cont, rotate, len1, len2;
+	char *temp, aux;
+	int	index1, cont, rotate, len1;
 
+	index1 = 0;
+	rotate = 0;
+	cont = 0;
 	if (ft_strlen(s1) != ft_strlen(s2))
 		return (0);
 	if (s1 == s2)
 		return (1);
 	if (!(temp = (char *)malloc(ft_strlen(s1))))
 		return (0);
-	index1 = 0;
-	rotate = 0;
-	len2 = 0;
-	cont = 0;
 	while (s2[index1])
 	{
 		temp[index1] = s2[index1];
@@ -36,28 +31,36 @@ int     hv_necklace(char *s1, char *s2)
 	while (index1 > 0)
 	{
 		if (temp[index1 - 1] == s1[index1 - 1])
-			index1--;
-		else
+		{
+			if (index1 - 1 == 0)
+				return (1);
+			else
+				;			
+		}
+		else if (cont < ft_strlen(s1))
 		{
 			cont++;
-			while (rotate < cont && cont < ft_strlen(s1))
+			while (rotate < cont && cont <= ft_strlen(s1))
 			{
 				len1 = ft_strlen(s1);
+				aux = temp[len1 - 1];
 				while (len1 > 0)
 				{
 					while (len1 - 1 > 0)
 					{
-						temp[len1 - 1] = s2[len1 - 2];
+						temp[len1 - 1] = temp[len1 - 2];
 						len1--;
 					}
 					len1--;
-					len2++;
 				}
-				temp[len1] = s2[len2];
+				temp[len1] = aux;
 				rotate++;
 				index1 = ft_strlen(s1) + 1;
 			}
 		}
+		else
+			return (0);
+		
 		index1--;		
 	}
 	return (0);
